5f8a7970b4f11f82e4eafe1f39f22ba3da61ed72
saberda <630991493@qq.com>
2020-06-19 15:11:01 -0400

    add function LIS

---
+++ Useful Code Library/Longest_Increasing_subsequence.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int lengthOfLTS(vector<int>& nums) {
+    if (nums.empty()) return 0;
+    vector<int> dp;
+    for (auto x : nums) {
+        int pos = lower_bound(dp.begin(), dp.end(), x) - dp.begin();
+        if (pos == dp.size()) dp.push_back(x);
+        else dp[pos] = x;
+    }
+    return dp.size();
+}

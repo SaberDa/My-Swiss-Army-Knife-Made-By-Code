538bd720ea54130dd53281caa0a273e6e0712dd7
saberda <630991493@qq.com>
2020-06-26 00:59:19 -0400

    add function declaration

--- Useful Code Library/Any/Any.hpp
+++ Useful Code Library/Any/Any.hpp
@@ -1,3 +1,9 @@
+/* 
+ * std: any是一种值类型，它能够更改其类型，同时仍然具有类型安全性。
+ * 也就是说，对象可以保存任意类型的值，但是它们知道当前保存的值是哪种类型。
+ * 在声明此类型的对象时，不需要指定可能的类型。 
+ */
+
 #ifndef __ANY_H__
 #define __ANY_H__
 
@@ -8,6 +14,10 @@
 class Any {
 public:
     Any() : m_typeIndex(std::type_index(typeid(void))) {}
+    Any(Any& other) : m_basePtr(other.clone()), m_typeIndex(other.m_typeIndex) {}
+    Any(Any&& other) : m_basePtr(std::move(other.m_basePtr)), m_typeIndex(m_typeIndex) {}
+
+    
 
 private:
     class Base;
